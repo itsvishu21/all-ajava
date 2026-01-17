f6ace5e75786d0c86c37a15551de8c20db1a4eb9
Vishu Thakur <vishuthakur251@gmail.com>
2026-01-17 22:37:53 +0530

    cleck prime number

--- Prime.java
+++ Prime.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 
 public class Prime {
-
-    // Method to check if a number is prime
     public static boolean isPrime(int n) {
-        // 0, 1, and negative numbers are not prime
         if (n <= 1) {
             return false;
         }
-
-        // Check for factors from 2 up to the square root of n
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
-                // If n is divisible by any number other than 1 and itself, it's not prime
                 return false;
             }
         }
-
-        // If no divisors were found, the number is prime
         return true;
     }
 
